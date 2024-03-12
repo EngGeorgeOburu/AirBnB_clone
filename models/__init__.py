0c6ac7902b24f471f4b9643783fbba5daaa11fbb
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-03-12 13:38:53 +0300

    Rreload

--- models/__init__.py
+++ models/__init__.py
@@ -3,9 +3,23 @@
 
 from models.base_model import BaseModel
 from models import BaseModel
-
-"""
+from .user import User
+from .state import State
+from .city import City
+from .amenity import Amenity
+from .review import Review
+from .place import Place
 from models.engine.file_storage import FileStorage
+
+classes = {
+        'BaseModel': BaseModel,
+        'User': User,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Review': Review,
+        'Place': Place,
+    }
+
 storage = FileStorage()
 storage.reload()
-"""
