43b18fd58da1a9d44d34226ff7c1cb5e3259c370
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-03-12 13:06:22 +0300

    H

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 """Initializing the package."""
+
+from models.base_model import BaseModel
+from models import BaseModel
+
+"""
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
+"""
